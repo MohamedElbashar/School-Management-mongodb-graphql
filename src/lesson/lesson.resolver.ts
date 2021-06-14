e91d998b131aeb64388b36b908c96185c58ba9bc
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-14 16:33:56 +0200

    add lesson resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  @Query((returns) => LessonType)
+  lesson() {
+    return {
+      id: 'asdasfkvm',
+      name: 'physics',
+      startingDate: new Date().toISOString(),
+      endingDate: new Date().toISOString(),
+    };
+  }
+}
