055065d72871b8d557f63e433d0340ed1ff29fb3
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-14 20:18:40 +0200

    getAll lessons

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -6,10 +6,17 @@ import { LessonType } from './lesson.type';
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(private lessonSrvice: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
     return this.lessonSrvice.getLesson(id);
   }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    this.lessonSrvice.getLessons();
+  }
+
   @Mutation()
   createLesson(
     @Args('createLessonInput') createLessonInput: CreateLessonInput,
